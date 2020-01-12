fb7bfca7f4d3f2948b128e49cfd661ecab7cc3ff
Ishimoto Shinobu <nagakamira@gmail.com>
2020-01-12 19:15:59 +0900

    add simple argument parser
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import argparse
+
 def main():
-	print("Hello, world!")
+	parser = argparse.ArgumentParser(description="Next generation package manager")
+
+	parser.add_argument("install", metavar="install", type=str, nargs='+')
+
+	parser.add_argument("-c", "--config", type=str, dest="config")
+	parser.add_argument("-r", "--rootdir", type=str, dest="rootdir")
+	parser.add_argument("-f", "--force", action="store_true", dest="force")
+
+	args = parser.parse_args()
+
+	print(args.config)
+
+	if args.force:
+		print("Force mode enabled!");
+
+	args.install.pop(0)
+	for i in args.install:
+		print(i)
 
 main()
