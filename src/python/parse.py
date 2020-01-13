34a4861f379f2f1a8f38d46bc34cc4c00e8d3816
Ishimoto Shinobu <nagakamira@gmail.com>
2020-01-13 22:11:02 +0900

    add json parser
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- src/python/parse.py
+++ src/python/parse.py
@@ -1,11 +1,29 @@
 import os
 import re
+import json
+
+def CheckFilePattern(file, pattern):
+	if file is None or not os.path.exists(file):
+		print("File wasn't specified or doesn't exist")
+		sys.exit(1)
+	if pattern is None:
+		print("Pattern name wasn't specified")
+		sys.exit(1)
 
 def CommentVariable(file, pattern):
+	CheckFilePattern(file, pattern)
 	with open(file, "r+") as kagamibuild:
 		text = kagamibuild.read().strip()
 		p = re.search("# %s:(.*)" % pattern, text)
-		p = p.group(0)
-		p = p.replace("# %s:" % pattern, "")
-		p = p.lstrip()
-		print(p)
+		if p != None:
+			p = p.group(0)
+			p = p.replace("# %s:" % pattern, "")
+			p = p.lstrip()
+			print(p)
+
+def JsonVariable(file, pattern):
+	CheckFilePattern(file, pattern)
+	with open(file, "r+") as jsonfile:
+		data = json.load(jsonfile)
+		if data != None:
+			print(data["%s" % pattern])
